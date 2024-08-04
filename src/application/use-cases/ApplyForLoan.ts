f3af7834c243c279af02621179d11cfc67c92961
Matheus Batista <mbatista.sarti@gmail.com>
2024-08-04 02:56:30 -0300

    :construction: add usecase ApplyForLoan

---
+++ src/application/use-cases/ApplyForLoan.ts
@@ -0,0 +1,31 @@
+import { Loan } from "../../domain/entities/Loan";
+import CalculateInstallements from "../../domain/services/CalculateInstallments";
+import ApplyForLoanInput from "../dtos/loan/ApplyForLoanInput";
+import ApplyForLoanOutput from "../dtos/loan/ApplyForLoanOutput";
+import IInstallmentRepository from "../repositories/IInstallmentRepository";
+import ILoanRepository from "../repositories/ILoanRepository";
+import UseCase from "./UseCase";
+
+export default class ApplyForLoan implements UseCase {
+  constructor(
+    readonly loanRepository: ILoanRepository // readonly installmentRepository: IInstallmentRepository
+  ) {}
+
+  async execute(input: ApplyForLoanInput): Promise<ApplyForLoanOutput> {
+    const loan = Loan.create(
+      input.userCpf,
+      input.userUf,
+      input.userBirthday,
+      input.total,
+      input.monthlyInstallment
+    );
+    const installments = CalculateInstallements.calculate(loan);
+    await this.loanRepository.save(loan);
+    // installments.forEach(
+    //   async (installment) => await this.installmentRepository.save(installment)
+    // );
+    return {
+      loanId: loan.id,
+    };
+  }
+}
