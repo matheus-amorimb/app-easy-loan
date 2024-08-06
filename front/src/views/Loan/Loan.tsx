3d57a0fbe52d7e356ad5be6a36c0b07f49f1ef5d
Matheus Batista <mbatista.sarti@gmail.com>
2024-08-06 15:53:23 -0300

    :sparkles: add view Loan

--- front/src/views/Loan/Loan.tsx
+++ front/src/views/Loan/Loan.tsx
@@ -1,11 +1,21 @@
-import Installment from '../../components/installment/Installment';
+import { useState } from 'react';
+import LoanForm from '../../components/LoanForm/LoanForm';
+import { SimulateLoanInput } from '../../models/SimulateLoanInput';
 import './Loan.css';
+import Installment from '../../components/Installment/Installment';
 
 function Loan() {
+  const [loanInput, setLoanInput] = useState<SimulateLoanInput | null>(null);
+
+  const handleFormSubmit = (data: SimulateLoanInput) => {
+    setLoanInput(data);
+  };
+
   return (
     <>
-      <h1 className="title">Simule e solicite o seu empréstimo.</h1>
-      <Installment />
+      <h1 className="title container">Simule e solicite o seu empréstimo.</h1>
+      <LoanForm onSubmit={handleFormSubmit} />
+      {loanInput && <Installment simulateLoanInput={loanInput} />}
     </>
   );
 }
