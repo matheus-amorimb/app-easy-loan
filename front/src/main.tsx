0adff4fb31ce6fa507497980258355ec0610695e
Matheus Batista <mbatista.sarti@gmail.com>
2024-08-05 23:54:16 -0300

    :wranche: files update

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Loan from './views/Loan/Loan.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Loan />
   </React.StrictMode>,
-)
+);
