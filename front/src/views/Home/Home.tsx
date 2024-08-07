13655f93382c782dae9950f6b001d00f6d0f5c78
Matheus Batista <mbatista.sarti@gmail.com>
2024-08-07 20:11:32 -0300

    :sparkles: improve home layout

---
+++ front/src/views/Home/Home.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useState } from 'react';
+import LoanForm from '../../components/LoanForm/LoanForm';
+import { SimulateLoanInput } from '../../models/SimulateLoanInput';
+import './Home.css';
+import Installment from '../../components/Installment/Installment';
+import logo from '../../assets/easy-loan-logo-header.svg';
+
+function Home() {
+  const [loanInput, setLoanInput] = useState<SimulateLoanInput | null>(null);
+
+  const handleFormSubmit = (data: SimulateLoanInput) => {
+    setLoanInput(data);
+  };
+
+  return (
+    <>
+      <header className="container header">
+        {' '}
+        <img src={logo} alt="" />
+        <a href="/loans">Meu empréstimos</a>
+      </header>
+      <h1 className="title container">Simule e solicite o seu empréstimo.</h1>
+      <LoanForm onSubmit={handleFormSubmit} />
+      {loanInput && <Installment simulateLoanInput={loanInput} />}
+    </>
+  );
+}
+
+export default Home;
