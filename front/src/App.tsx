e845bbe97c63a00d8c0d1c9c0c52aa24ab33b7ac
Matheus Batista <mbatista.sarti@gmail.com>
2024-08-07 20:08:07 -0300

    :sparkles: create routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,35 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Loan from './views/Loan/Loan';
+import Home from './views/Home/Home';
+import Private from './routes/Private';
+import SignIn from './views/SignIn/SignIn';
+import SignUp from './views/SignUp/SignUp';
+import Loan from './views/Loans/Loan';
 
-export default function App() {
+const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signin" element={<Loan />}></Route>
-        <Route path="/signup" element={<Loan />}></Route>
-        <Route path="/" element={<Loan />}></Route>
-        <Route path="*" element={<Navigate to={'/signin'} />}></Route>
+        <Route path="/sign_in" element={<SignIn />}></Route>
+        <Route path="/sign_up" element={<SignUp />}></Route>
+        <Route
+          path="/home"
+          element={<Private>{<Home></Home>}</Private>}
+        ></Route>
+        <Route
+          path="/loans"
+          element={<Private>{<Loan></Loan>}</Private>}
+        ></Route>
+        <Route
+          path="*"
+          element={
+            <Private>
+              <Navigate to="/home" />
+            </Private>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
-}
+};
+
+export default App;
